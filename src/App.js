85f729daf23e7e198507f24a6a9995dba75ee565
abin02 <abinchandran2002@gmail.com>
2022-11-20 16:25:23 +0530

    initial setup for project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
